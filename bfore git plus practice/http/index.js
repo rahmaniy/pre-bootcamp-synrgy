73e343e4157ff11b6cf45ef5275a510e2319c4f8
rahmaniy <nrahmaniyanti@gmail.com>
2022-01-31 21:37:37 +0700

    latian http

--- bfore git plus practice/http/index.js
+++ bfore git plus practice/http/index.js
@@ -14,4 +14,14 @@ function onRequest(request, response) {
     })
 }
 
+function jsonRequest(request, response) {
+    response.writeHead(200, {"Content-Type" : "application/json"})
+    const data = {
+        name: "Yudi Krisnandi",
+        age: 22
+    }
+    response.end(JSON.stringify(data))
+}
+
 http.createServer(onRequest).listen(8000)
+http.createServer(jsonRequest).listen(443)
